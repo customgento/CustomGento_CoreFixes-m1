4fc8d50e25325677cf1024c51c2b2a7d187265c4
Simon Sprankel <simon@customgento.com>
2018-03-26 15:58:45 +0200

    added bugfix for incorrect price calculation if qty > available qty

---
+++ app/code/community/CustomGento/CoreFixes/Model/Sales/Quote/Address/Total/README.md
@@ -0,0 +1,7 @@
+# Description
+This overwrite fixes a bug, which leads to price reductions on each page reload in the cart when the qty of the product in the cart exceeds the available qty and backorders are disabled.
+
+# Resources
+* https://magento.stackexchange.com/q/164640/142
+* https://magento.stackexchange.com/q/147763/142
+* https://community.magento.com/t5/Technical-Issues/Cart-price-gets-deducted-on-page-reload-when-product-qty-is-more/td-p/55777
---
+++ app/code/community/CustomGento/CoreFixes/Model/Sales/Quote/Address/Total/Subtotal.php
@@ -0,0 +1,56 @@
+<?php
+
+class CustomGento_CoreFixes_Model_Sales_Quote_Address_Total_Subtotal
+    extends Mage_Sales_Model_Quote_Address_Total_Subtotal
+{
+    /**
+     * @inheritdoc
+     */
+    protected function _initItem($address, $item)
+    {
+        if ($item instanceof Mage_Sales_Model_Quote_Address_Item) {
+            $quoteItem = $item->getAddress()->getQuote()->getItemById($item->getQuoteItemId());
+        }
+        else {
+            $quoteItem = $item;
+        }
+        $product = $quoteItem->getProduct();
+        $product->setCustomerGroupId($quoteItem->getQuote()->getCustomerGroupId());
+
+        /**
+         * Quote super mode flag mean what we work with quote without restriction
+         */
+        if ($item->getQuote()->getIsSuperMode()) {
+            if (!$product) {
+                return false;
+            }
+        }
+        else {
+            if (!$product || !$product->isVisibleInCatalog()) {
+                return false;
+            }
+        }
+
+        if ($quoteItem->getParentItem() && $quoteItem->isChildrenCalculated()) {
+            $finalPrice = $quoteItem->getParentItem()->getProduct()->getPriceModel()->getChildFinalPrice(
+                $quoteItem->getParentItem()->getProduct(),
+                $quoteItem->getParentItem()->getQty(),
+                $quoteItem->getProduct(),
+                $quoteItem->getQty()
+            );
+            $item->setPrice($finalPrice)
+                ->setBaseOriginalPrice($finalPrice);
+            $item->calcRowTotal();
+        } else if (!$quoteItem->getParentItem()) {
+            $finalPrice = $product->getFinalPrice($quoteItem->getQty());
+            $item->setPrice($finalPrice)
+                ->setBaseOriginalPrice($finalPrice);
+            $item->calcRowTotal();
+            $this->_addAmount($item->getRowTotal());
+            $this->_addBaseAmount($item->getBaseRowTotal());
+            $address->setTotalQty($address->getTotalQty() + $item->getQty());
+        }
+
+        return true;
+    }
+}
