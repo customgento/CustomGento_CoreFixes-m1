de97773bd886e7a2af713514abb052bef222b7b4
Simon Sprankel <simon@customgento.com>
2020-07-15 16:34:07 +0200

    Fix TinyMCE iframe removal issue

---
+++ js/customgento/corefixes/tiny_mce/iframe_fix.js
@@ -0,0 +1,7 @@
+tinyMceWysiwygSetup.prototype.initialize = tinyMceWysiwygSetup.prototype.initialize.wrap(function (parentMethod, htmlId, config) {
+    parentMethod(htmlId, config);
+    if (typeof tinymce === 'object' && typeof tinymce.util === 'object' && typeof tinymce.util.JSON === 'object') {
+        // make sure that JSON is globally available, because it is used in js/tiny_mce/plugins/media/editor_plugin.js
+        window.JSON = tinymce.util.JSON;
+    }
+});
