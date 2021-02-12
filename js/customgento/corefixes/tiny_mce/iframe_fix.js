96c28aec1326bf4cdd483242c6cdc40767cf5672
Simon Sprankel <sprankhub@users.noreply.github.com>
2021-02-12 08:09:08 +0100

    Extend object instead of replacing it
    Fixes issue "JSON.stringify is not a function"

--- js/customgento/corefixes/tiny_mce/iframe_fix.js
+++ js/customgento/corefixes/tiny_mce/iframe_fix.js
@@ -2,6 +2,6 @@ tinyMceWysiwygSetup.prototype.initialize = tinyMceWysiwygSetup.prototype.initial
     parentMethod(htmlId, config);
     if (typeof tinymce === 'object' && typeof tinymce.util === 'object' && typeof tinymce.util.JSON === 'object') {
         // make sure that JSON is globally available, because it is used in js/tiny_mce/plugins/media/editor_plugin.js
-        window.JSON = tinymce.util.JSON;
+        window.JSON = Object.assign(window.JSON, tinymce.util.JSON);
     }
 });
